4996c0dc1908ab96589eba132d9a7fdd2131f23c
Jas2005-ct <jaswantjas02@gmail.com>
2026-05-17 14:44:53 +0530

    feat: initialize Django project with Supabase storage and responsive home template

--- core/settings.py
+++ core/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
